1b1481cfe8db6c106085d89fcb3b61873b687bd1
Wang-Kai <persistence2013@sina.com>
2018-08-26 23:12:07 +0800

    A: add merge sort

--- sort.go
+++ sort.go
@@ -5,11 +5,64 @@ import (
 )
 
 func main() {
-	var arr = []int{12, 3, 56, 7, 9, 24, 2, 2, 390, 42, 34}
+	var arr = []int{51, 30, 235, 75, 9, 24, 42, 486, 0, 190}
 
-	InsertionSort(arr)
+	// InsertionSort(arr)
+	MergeSort(arr, 0, len(arr)-1)
+
 	fmt.Println(arr)
 }
+
+/*
+	Merger sort
+*/
+func MergeSort(arr []int, from, to int) {
+	if from < to {
+		halfTag := (from + to) / 2
+		MergeSort(arr, from, halfTag)
+		MergeSort(arr, halfTag+1, to)
+		merge(arr, from, halfTag, to)
+	}
+}
+
+/*
+	Merge sort
+*/
+
+// merge 是归并排序的一个子程序，负责排序数组中已经排序好的两段数组
+// [p, q]是已经排序好的一段数字，(q, r] 是一段已经排序好的数字，index 从 0 开始
+func merge(arr []int, p, q, r int) {
+	leftArr := make([]int, q-p+1)
+	rightArr := make([]int, r-q)
+
+	copy(leftArr, arr[p:q+1])
+	copy(rightArr, arr[q+1:])
+
+	var i, j int
+
+	for k := p; k <= r; k++ {
+		if leftArr[i] > rightArr[j] {
+			arr[k] = rightArr[j]
+			j++
+
+			if j >= len(rightArr) {
+				// 将 leftArr 中剩余的项全部填到 arr 尾部，结束循环
+				copy(arr[k+1:], leftArr[i:])
+				break
+			}
+		} else {
+			arr[k] = leftArr[i]
+			i++
+
+			if i >= len(leftArr) {
+				copy(arr[k+1:], rightArr[j:])
+				break
+			}
+		}
+	}
+}
+
+//-------------------------------------------------------
 
 /*
 	insert sort
